793144ce715a267e1fc6d471ef25419e08acb607
Meuss <thomas.miller147@gmail.com>
2018-01-27 00:52:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# vue-calculator
+
+> Learning me some more Vue.js
+
+![OSX calculator](https://cdn.cultofmac.com/wp-content/uploads/2014/08/Screen-Shot-2014-08-04-at-5.59.53-PM-640x406.jpg)
+
+## Recreating a pixel-perfect OSX calculator
+
+Goals:
+
+- Keep learning Vue.js
+- Managing state better with Vuex
+- Customizing Webpack
+
+
---
+++ src/components/Numbers.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="numbers">
+    <div v-for="number in numbers" :key="number" class="number">
+      {{number}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.numbers {
+  display: grid;
+  grid-template-rows: repeat(5, 50px);
+  grid-template-columns: repeat(3, 60px);
+  grid-gap: 1px;
+}
+.number {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
+}
+</style>
